01587053ac0912489cbfc7c52ed44de8f324beca
Douglas <kaloblian@gmail.com>
2026-05-25 18:18:37 -0300

    feat(p2): implementa busca por capital

--- App.tsx
+++ App.tsx
@@ -4,8 +4,11 @@ import restCountries from './utils/restCountries';
 
 export default function App() {
 
-  //Caputura o que o usuário digita no campo
+  //Caputura o que o usuário digita no campo país
   const [pais, setPais] = useState('');
+  //Captura o que o usuário digita no campo capital
+  const [capital, setCapital] = useState('');
+
   //Salva os dados necessários que foram extraidos da API
   const [detalhes, setDetalhes] = useState({
     nomeComum: null,
@@ -13,11 +16,20 @@ export default function App() {
     nomeRusso: null,
     imagemPais: null
   });
+
+  const [info, setInfo] = useState({
+    nomeComum: null,
+    bandeira: null
+  });
+
+
   //State para mostrar ou não as informações por busca por país
   const [exibirListaPorPais, setExibirListaPorPais] = useState(false);
+  //State para mostrar ou não as informações por busca por capital
+  const [exibirListaPorCapital, setExibirListaPorCapital] = useState(false);
  
   //Sempre que apertar o botão, puxa os dados da API e salva na variável
-  const buscaPorNome = () => {
+  const buscaPorPais = () => {
       restCountries.get(`/name/${pais}`)
       .then((result) => {
           setDetalhes({
@@ -26,6 +38,7 @@ export default function App() {
             nomeRusso: result.data[0].translations.rus.common,
             imagemPais: result.data[0].maps.openStreetMaps
           });
+          console.log(JSON.stringify(result.data[0], null, 2));
           setExibirListaPorPais(true)
         })
         .catch((error) => {
@@ -33,43 +46,88 @@ export default function App() {
           setExibirListaPorPais(false);
         });
       };
-  
+
+  const buscaPorCapital = () => {
+      restCountries.get(`/capital/${capital}`)
+      .then((result) => {
+          setInfo({
+            nomeComum: result.data[0].name.common,
+            bandeira: result.data[0].flags.png,
+          });
+          console.log(JSON.stringify(result.data[0], null, 2));
+          setExibirListaPorCapital(true)
+        })
+        .catch((error) => {
+          alert('País não encontrado! Lembre-se de digitar em inglês.')
+          setExibirListaPorCapital(false);
+        });
+      };
   
 
   return (     
         <View style={styles.container}>
-          <View style={styles.firstGridTop}>
-            <Text 
-              style={styles.title}>
-              Digite um país para consultar suas informações completas
-            </Text>
-            <Text
-              style={styles.subtitle}>
-                OBS: o nome do país deve ser em inglês
-            </Text>
-              <View style={styles.firstGridBotton}>  
-                <TextInput
-                  style={styles.input}
-                  placeholder='Digite aqui..'
-                  onChangeText={setPais}
-                  value={pais}
-                  />
-                  <Pressable
-                    style={styles.button} 
-                    onPress={buscaPorNome}
-                    >
-                    <Text
-                      style={styles.buttonTxt}>Buscar
-                    </Text>
-                  </Pressable>
-                </View>
+
+          <View style={styles.topGrid}>
+            <View style={styles.firstGridTop}>
+              <Text 
+                style={styles.title}>
+                Digite um país para consultar suas informações completas
+              </Text>
+              <Text
+                style={styles.subtitle}>
+                  OBS: o nome do país deve ser em inglês
+              </Text>
+                <View style={styles.firstGridBotton}>  
+                  <TextInput
+                    style={styles.input}
+                    placeholder='Digite aqui..'
+                    onChangeText={setPais}
+                    value={pais}
+                    />
+                    <Pressable
+                      style={styles.button} 
+                      onPress={buscaPorPais}
+                      >
+                      <Text
+                        style={styles.buttonTxt}>Buscar
+                      </Text>
+                    </Pressable>
+                  </View>
+            </View>
+
+            <View style={styles.firstGridTop}>
+              <Text 
+                style={styles.title}>
+                Digite uma capital para consultar suas informações de seu País
+              </Text>
+              <Text
+                style={styles.subtitle}>
+                  OBS: o nome da capital deve ser em inglês
+              </Text>
+                <View style={styles.firstGridBotton}>  
+                  <TextInput
+                    style={styles.input}
+                    placeholder='Digite aqui..'
+                    onChangeText={setCapital}
+                    value={capital}
+                    />
+                    <Pressable
+                      style={styles.button} 
+                      onPress={buscaPorCapital}
+                      >
+                      <Text
+                        style={styles.buttonTxt}>Buscar
+                      </Text>
+                    </Pressable>
+                  </View>
+            </View>
           </View>
 
           {exibirListaPorPais && (
           <View style={styles.list}>
 
             <Text style={styles.listTitle}>
-              Informações Detalhadas
+              Resultado por País
             </Text>
             
             <View style={styles.card}>
@@ -109,6 +167,35 @@ export default function App() {
 
           </View>
           )}
+
+          {exibirListaPorCapital && (
+          <View style={styles.list}>
+
+            <Text style={styles.listTitle}>
+              Resultado por Capital
+            </Text>
+
+            <View style={styles.card}>
+
+              <View style={styles.listItem}>
+                <Text style={styles.itemTitle}>Nome do País</Text>
+                <Text style={styles.valor}>
+                  {info.nomeComum || 'Não possui'}
+                </Text>
+              </View>
+
+              <View style={styles.listItem}>
+                <Text style={styles.itemTitle}>Bandeira</Text>
+                {info.bandeira && (
+                  <Image
+                    source={{ uri: info.bandeira }}
+                    style={{ width: 200, height: 120 }}/>
+                )}
+              </View>
+
+            </View>
+          </View>
+        )}
 
         </View>
   );
@@ -206,6 +293,10 @@ const styles = StyleSheet.create({
   title: {
     fontWeight: 'bold',
     fontSize: 16
+  },
+  topGrid: {
+    flexDirection: 'row',
+    gap: 15
   },
   valor: {
     fontSize: 17,
